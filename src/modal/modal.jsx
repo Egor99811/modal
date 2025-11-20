f2d15b9e6e07dff8b47beabd4034d0fdd953356b
Egor99811 <kurilkin.y@mail.ru>
2025-11-20 15:05:27 +0300

    add modal

---
+++ src/modal/modal.jsx
@@ -0,0 +1,13 @@
+import { createPortal } from "react-dom"
+import './modal.css'
+
+export function Modal({title, closeModal, children}) {
+    return createPortal(
+        <div className="modal">
+            <h1>{title}</h1>
+            {children}
+            <button onClick={() => closeModal(false)}>Close</button>
+        </div>,
+        document.getElementById('modal')
+    )
+}
